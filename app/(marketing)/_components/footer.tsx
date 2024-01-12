010fad0626bf90b0741f904c4db0ffe256f39ed0
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2024-01-11 19:11:45 -0600

    add Marketing Page

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,21 @@
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 w-full px-4 py-2 border-t bg-slate-100">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-center w-full">
+          <Button size="sm" variant="ghost">
+            Privacy Policy
+          </Button>
+          <Button size="sm" variant="ghost">
+            Terms of Service
+          </Button>
+        </div>
+      </div>
+    </footer>
+  );
+};
