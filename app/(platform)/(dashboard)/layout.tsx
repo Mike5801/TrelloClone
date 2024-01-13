895d9cc5f383df43b1b5982c0637bbf937cf48bd
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2024-01-12 19:06:05 -0600

    add organization layer and middleware

---
+++ app/(platform)/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "./_components/navbar";
+
+const DashboardLayout = ({ children } : { children: React.ReactNode }) => {
+  return (
+    <main className="h-full">
+      <Navbar />
+      {children}
+    </main>
+  );
+};
+
+export default DashboardLayout;
